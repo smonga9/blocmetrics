c28ae78454cf110922146d240c831077f9369d24
Stutay Monga <Stutay.Monga9@gmail.com>
2018-08-06 20:42:50 -0400

    Checkpoint 6 Completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :registeredapps
 
+  namespace :api, defaults: { format: :json } do
+  	match '/events', to: 'events#preflight', via: [:options]
+  	resources :events, only: [:create]
+
   devise_for :users, controllers: { confirmations: 'confirmations' }
 end
